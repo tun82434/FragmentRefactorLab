77acaa37a037ac19b3734cc170360a6e0bb78993
tun82434 <tun82434@temple.edu>
2025-03-12 15:34:45 -0400

    finished

--- app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
@@ -10,19 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val displayTextView = findViewById<TextView>(R.id.displayTextView)
-        val nameEditText = findViewById<EditText>(R.id.nameEditText)
-        val changeButton = findViewById<Button>(R.id.changeButton)
-
-        changeButton.setOnClickListener {
-            val name = nameEditText.text
-
-            displayTextView.text = if (name.isNotBlank()) {
-                "Hello, $name!"
-            } else {
-                "Please enter your name"
-            }
-        }
     }
 }
---
+++ app/src/main/java/edu/temple/fragmentrefactor/RefactorFragment.kt
@@ -0,0 +1,38 @@
+package edu.temple.fragmentrefactor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class RefactorFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_refactor, container, false).apply {
+            val displayTextView = findViewById<TextView>(R.id.displayTextView2)
+            val nameEditText = findViewById<EditText>(R.id.nameEditText2)
+            val changeButton = findViewById<Button>(R.id.changeButton2)
+
+            changeButton.setOnClickListener {
+                val name = nameEditText.text
+
+                displayTextView.text = if (name.isNotBlank()) {
+                    "Hello, $name!"
+                } else {
+                    "Please enter your name"
+                }
+            }
+        }
+    }
+
+
+}
